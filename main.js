d0fdc1390bb6e4cad22c938e3eb5b9c614889d6c
FatimaZelay <fatima.elay@gmail.com>
2025-01-16 16:02:41 +0100

    Animaties met Keyframe en Js

--- main.js
+++ main.js
@@ -45,3 +45,171 @@ interaction.classList.toggle('shake')
 }
 
 }
+
+
+
+/*Animatie N 2 */
+{ 
+  
+  
+  let interaction = document.querySelector('a:nth-of-type(2)')
+ 
+  
+  interaction.addEventListener('click', bounceOut)
+  
+  function bounceOut() {
+  
+  
+  interaction.classList.toggle('bounce')
+  
+  }
+  
+  }
+
+  /*Animatie N 3 */
+
+{ 
+  
+  let interaction = document.querySelector('a:nth-of-type(3)')
+  
+ 
+  interaction.addEventListener('click', SwingIn)
+  
+  function SwingIn() {
+  
+  
+  interaction.classList.toggle('swing')
+  
+  }
+  
+  }
+
+   /*Animatie N 4 */
+
+{ 
+  
+  let interaction = document.querySelector('a:nth-of-type(4)')
+  
+ 
+  interaction.addEventListener('click', FadeOutLeft)
+  
+  function FadeOutLeft() {
+  
+  
+  interaction.classList.toggle('fadingoutleft')
+  
+  }
+  
+  }
+
+   /*Animatie N 5 */
+
+   { 
+  
+    let interaction = document.querySelector('a:nth-of-type(5)')
+    
+   
+    interaction.addEventListener('click', Bouncing)
+    
+    function Bouncing() {
+    
+    
+    interaction.classList.toggle('bounceIn')
+    
+    }
+    
+    }
+
+    /*Animatie N 6 */
+
+   { 
+  
+    let interaction = document.querySelector('a:nth-of-type(6)')
+    
+   
+    interaction.addEventListener('click', fadeOutRight)
+    
+    function fadeOutRight() {
+    
+    
+    interaction.classList.toggle('fadeOutRight')
+    
+    }
+    
+    }
+
+ /*Animatie N 7 */
+
+ { 
+  
+  let interaction = document.querySelector('a:nth-of-type(7)')
+  
+ 
+  interaction.addEventListener('click', shakeY)
+  
+  function shakeY() {
+  
+  
+  interaction.classList.toggle('shakeY')
+  
+  }
+  
+  }
+
+
+/*Animatie N 8 */
+
+{ 
+  
+  let interaction = document.querySelector('a:nth-of-type(8)')
+  
+ 
+  interaction.addEventListener('click', LookAtMe)
+  
+  function LookAtMe() {
+  
+  
+  interaction.classList.toggle('LookAtMe')
+  
+  }
+  
+  }
+
+
+
+/*Animatie N 9 */
+{ 
+  
+  let interaction = document.querySelector('a:nth-of-type(9)')
+  
+ 
+  interaction.addEventListener('click', rubberBand)
+  
+  function rubberBand() {
+  
+  
+  interaction.classList.toggle('rubberBand')
+  
+  }
+  
+  }
+
+
+
+/*Animatie N 10 */
+
+{ 
+  
+  let interaction = document.querySelector('a:nth-of-type(10)')
+  
+ 
+  interaction.addEventListener('click', RotateiT)
+  
+  function RotateiT() {
+  
+  
+  interaction.classList.toggle('RotateiT')
+  
+  }
+  
+  }
